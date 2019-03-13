cda5bf8f44711a73130b357d6ac1372cbca70a44
huaiyohu0 <670518964@qq.com>
2019-03-13 18:15:16 +0800

    更新

---
+++ src/utils/request.js
@@ -0,0 +1,26 @@
+function req_get(url, data, callback) {
+    let HOST_URL = process.env.HOST_URL;
+
+    let patt = /http/;
+    if (!patt.test(url)) {
+        url = HOST_URL + url;
+    }
+
+    if (data) {
+        let urlParamArr = [];
+        for (let key in data) {
+            urlParamArr.push(key + '=' + data[key]);
+        }
+        let urlParam = '?' + urlParamArr.join('&');
+
+        url += urlParam;
+    }
+
+    fetch(url).then(response => response.json()).then(json => {
+        typeof callback == "function" && callback(json.data);
+    });
+}
+
+export {
+    req_get
+};
