53227523252669ef5adc2178aee508918f0db2d5
huaiyohu0 <670518964@qq.com>
2019-03-13 17:29:13 +0800

    更新

--- src/view/login/Login.vue
+++ src/view/login/Login.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="container">
         <div class="box-cc login-btn">
-            <van-button block round plain size="normal" type="primary" :loading="loading" loading-text="正在登陆" @click="doLogin">微信一键登陆</van-button>
+            <van-button v-if="isWXBrowser" block round plain size="normal" type="primary" :loading="loading" loading-text="正在登陆"
+                        @click="doLogin">微信一键登陆
+            </van-button>
         </div>
         <footer-cc></footer-cc>
     </div>
 </template>
 
 <script>
-    import {Button} from 'vant';
+    import Vue from 'vue';
+    import {Button, Notify} from 'vant';
+    import {Cookies} from 'js-cookie';
+
+    Vue.use(Notify);
 
     export default {
         name: "login",
@@ -17,36 +23,62 @@
             [Button.name]: Button,
         },
 
+        computed:{
+
+        },
+
         data() {
-            return{
-                loading: false
+            return {
+                loading: false,
+                openid: null,
+                isWXBrowser: false
             }
         },
 
-        methods:{
-            doLogin(){
+        created() {
+            this.checkWXBrowser();
+            this.openid = Cookies.get('openid');
+        },
+
+        methods: {
+            doLogin() {
+                if (!this.openid) {
+                    Notify('请在微信浏览器中进行登录授权~');
+                    return true;
+                }
                 this.loading = true;
+            },
+            checkWXBrowser () {
+                //window.navigator.userAgent属性包含了浏览器类型、版本、操作系统类型、浏览器引擎类型等信息，这个属性可以用来判断浏览器类型
+                var ua = window.navigator.userAgent.toLowerCase();
+                //通过正则表达式匹配ua中是否含有MicroMessenger字符串
+                if (ua.match(/MicroMessenger/i) == 'micromessenger') {
+                    this.isWXBrowser = true;
+                } else {
+                    Notify('请在微信浏览器中打开本网站');
+                    this.isWXBrowser = false;
+                }
             }
         },
 
-        beforeCreate: function() {
+        beforeCreate: function () {
             document.body.className = 'bg-cc';
         },
 
-        beforeDestroy: function() {
-            document.body.removeAttribute("class","bg-cc");
+        beforeDestroy: function () {
+            document.body.removeAttribute("class", "bg-cc");
         }
     }
 </script>
 
 <style type="text/css">
-    .bg-cc{
+    .bg-cc {
         width: 100%;
         height: -webkit-fill-available;
         background-image: url('../../assets/guide.png');
-        background-repeat:no-repeat;
-        background-size:100% 100%;
-        -moz-background-size:100% 100%;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        -moz-background-size: 100% 100%;
     }
 
     .login-btn {
